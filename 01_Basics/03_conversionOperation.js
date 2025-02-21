4f44586d0ac2fa19f0a30edad4e9cda86a355569
Yash Pawar <112814699+Yash-Pawar-1410@users.noreply.github.com>
2025-02-21 10:30:10 +0000

    Converstion of data

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,65 @@
+let score = 33
+let score1 = "33"
+let score2 = "22ss"
+let score3 = null
+let score4 = undefined
+let score5 = false
+
+console.log(typeof score);
+console.log(typeof (score1));
+
+console.log("----------------------------");
+
+
+let valueNumber = Number (score1)
+console.log(typeof (valueNumber));
+console.log("----------------------------");
+
+let valueNumber1 = Number (score2)
+console.log(typeof (valueNumber1));
+console.log(valueNumber1);
+console.log("----------------------------");
+
+console.log(score3);
+let valueNumber3=Number(score3)
+console.log(typeof (valueNumber3));
+console.log(valueNumber3);
+console.log("----------------------------");
+
+console.log(score4);
+let valueNumber4 = Number(score4)
+console.log(typeof (valueNumber4));
+console.log(valueNumber4);
+console.log("----------------------------");
+
+console.log(score5);
+let valueNumber5 = Number (score5)
+console.log(typeof (valueNumber5));
+console.log(valueNumber5);
+console.log("----------------------------");
+
+// "33" : 33
+// "33abe" : NaN (not a number but type is number)
+// true : 1 , false: 0
+
+
+let isLoggedIn =1 
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let isChar ="fa"
+let bolleanIsChar = Boolean (isChar)
+console.log(bolleanIsChar);
+
+// 1= true, 0=false
+// "" = false, " " = true
+
+console.log("----------------------------");
+
+let someNumber =21
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
