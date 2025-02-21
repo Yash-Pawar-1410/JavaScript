5228f23b77d3f62ec8e1955da494e5f792a3e219
Yash Pawar <112814699+Yash-Pawar-1410@users.noreply.github.com>
2025-02-21 09:52:00 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3)  // we are using node js, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+    // alert mdn : documentation (mozilla)
+    // tc39.es : original documentation of JS
+
+    let name="Yash"
+    let age = 25
+    let isloggedIn = false 
+
+    // number = 2 to power 53
+    // bigint 
+    // string = " " or ' '
+    // boolean = true or false
+    // null : standalone value (empty value)
+    // undefined : value not assign
+    // symbol: unique
+
+
+    // object
+
+
+    //typeof : to the the datetype
+    console.log(typeof "yash"); // string
+    console.log(typeof age); // number
+    console.log(typeof null); //object 
+    console.log(typeof undefined); // undefined
+    
+    
+    
