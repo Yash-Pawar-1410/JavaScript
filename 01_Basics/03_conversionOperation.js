0716115105297abdd4972c65e2f87179d5eba8d9
Yash Pawar <112814699+Yash-Pawar-1410@users.noreply.github.com>
2025-02-22 07:21:11 +0000

    Other Conversation Operation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -4,7 +4,7 @@ let score2 = "22ss"
 let score3 = null
 let score4 = undefined
 let score5 = false
-
+/*
 console.log(typeof score);
 console.log(typeof (score1));
 
@@ -36,13 +36,12 @@ console.log(score5);
 let valueNumber5 = Number (score5)
 console.log(typeof (valueNumber5));
 console.log(valueNumber5);
-console.log("----------------------------");
-
+console.log("----------------------------");  */
 // "33" : 33
 // "33abe" : NaN (not a number but type is number)
 // true : 1 , false: 0
 
-
+/*
 let isLoggedIn =1 
 let booleanIsLoggedIn = Boolean (isLoggedIn)
 console.log(booleanIsLoggedIn);
@@ -51,15 +50,59 @@ console.log(booleanIsLoggedIn);
 let isChar ="fa"
 let bolleanIsChar = Boolean (isChar)
 console.log(bolleanIsChar);
+*/
 
 // 1= true, 0=false
 // "" = false, " " = true
 
-console.log("----------------------------");
+/* console.log("----------------------------");
 
 let someNumber =21
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/
+
+// ************** Operations *************
+
+/*let value = 3
+let negValue = - value
+console.log(negValue);
+
+console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);*/
+
+let str1 = "Yash"
+let str2 = "Pawar"
+let str3 = 2
+let str4 = 4
+
+/*
+console.log(str1 + str3);    // Yash2
+console.log(str4 + str2);    // 4Pawar
+console.log(str1 + str2);    // YashPawar
+console.log(str1 + str3 + str4);  //Yash24
+console.log(str3 + str4 + str2);  // 6Pawar    
+ */
+
+ // don't use like this
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+// gameCounter++
+++gameCounter
+console.log(gameCounter);
+
+
+
 
 
